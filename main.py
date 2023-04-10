4146b0c18e1137a4c818b30e5445eff414a76b7f
Anderson T <tseng.andersonn@gmail.com>
2023-04-10 08:59:49 -0700

    more utility

--- main.py
+++ main.py
@@ -9,42 +9,56 @@ from parsers.SemesterParser import SemesterParser
 from parsers.TransferParser import TransferParser
 from parsers.AttributesParser import AttributesParser
 
-def parse_build_data():
-    s = SemesterParser()
-    s.loadParseSaveAll()
+"""
+Force fetching data from the internet.
+This may take some time and is likely not neccessary.
+"""
     
+
+"""
+Parse all data.
+May fetch content from the internet if not found in data/
+May take some time.
+"""
+def parse_build_data(getFromWeb = False):
+    SemesterParser.loadParseSaveAll(getPagesFromWeb=getFromWeb)
+
     p = AttributesParser()
-    p.LoadParseAndSave()
+    p.LoadParseAndSave(getPageFromWeb=getFromWeb)
     
     c = CatalogueParser()
-    c.LoadParseAndSave()
+    c.LoadParseAndSave(getPageFromWeb=getFromWeb)
+    
+    # The process for fetching transfer data is currently
+    # very manual and is not automated.
     
     #TransferParser.transfer_courses_generator()
     #TransferParser.ahk_data_generator()
     TransferParser.parse_and_save_transfer_pdfs()
     
+    
     c = CourseInfoBuilder()
     c.hydrateBuildSave()
 
 # refresh current semester every 24 hours
-# TODO: implement smarter way to keep data updated
 def daily_update():
     logging.info("Fetching fresh course data from the Langara website.")
-    
     #p = SemesterParser(2023, 20)
     #p.loadPageFromWeb()
     #p.parseAndSave()
     
 
-if not os.path.exists("data/json/"):
-    print("No data found. Downloading and parsing all course data.")
-    SemesterParser.loadParseSaveAll()
-    
 
+
+
+if not os.path.exists("data/json/"):
+    print("No data found. Downloading and parsing all data.")
+    parse_build_data(getFromWeb=True)
+    
 daily_update()
 schedule.every().day.at("00:00:00").do(daily_update)
 
-# uvicorn api.Api:app --host localhost --reload
+# uvicorn api.Api:app --host localhost --port 5000 --reload
 uvicorn.run("api.Api:app", host="0.0.0.0", port=5000)
 
 
