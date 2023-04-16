0edb1059e2b5ae2c4e1c6f00256fb6fce101a6ee
Anderson T <tseng.andersonn@gmail.com>
2023-04-16 05:15:25 -0700

    refactor

---
+++ build.py
@@ -0,0 +1,50 @@
+import sys
+
+
+from builders.AllBuilder import AllBuilder
+from builders.CourseInfoBuilder import CourseInfoBuilder
+from parsers.CatalogueParser import CatalogueParser
+from parsers.SemesterParser import SemesterParser
+from parsers.TransferParser import TransferParser
+from parsers.AttributesParser import AttributesParser
+
+
+"""
+Parse all data.
+May fetch content from the internet if not found in data/
+May take some time.
+"""
+def parse_build_data(getFromWeb = False):
+    SemesterParser.loadParseSaveAll(getPagesFromWeb=getFromWeb)
+
+    p = AttributesParser()
+    p.LoadParseAndSave(getPageFromWeb=getFromWeb)
+    
+    c = CatalogueParser()
+    c.LoadParseAndSave(getPageFromWeb=getFromWeb)
+    
+    # The process for fetching transfer data is currently
+    # very manual and is not automated.
+    
+    #TransferParser.transfer_courses_generator()
+    #TransferParser.ahk_data_generator()
+    TransferParser.parse_and_save_transfer_pdfs()
+    
+    
+    c = CourseInfoBuilder()
+    c.hydrateBuildSave()
+    
+    a = AllBuilder()
+    a.hydrateBuildSave()
+
+def UPDATE_ALL(): 
+    p = SemesterParser(2023, 20)
+    p.loadPageFromWeb()
+    p.parseAndSave()
+
+    parse_build_data()
+    sys.exit()
+    
+# WARNING: this takes 9 minutes and 30 seconds to finish on my laptop
+# TODO: make this faster (?)
+UPDATE_ALL() 
--- main.py
+++ main.py
@@ -2,67 +2,17 @@ import os
 import logging
 import uvicorn
 import schedule
-import sys
 
-from builders.AllBuilder import AllBuilder
-from builders.CourseInfoBuilder import CourseInfoBuilder
-from parsers.CatalogueParser import CatalogueParser
 from parsers.SemesterParser import SemesterParser
-from parsers.TransferParser import TransferParser
-from parsers.AttributesParser import AttributesParser
 
-"""
-Force fetching data from the internet.
-This may take some time and is likely not neccessary.
-"""
-    
-
-"""
-Parse all data.
-May fetch content from the internet if not found in data/
-May take some time.
-"""
-def parse_build_data(getFromWeb = False):
-    SemesterParser.loadParseSaveAll(getPagesFromWeb=getFromWeb)
-
-    p = AttributesParser()
-    p.LoadParseAndSave(getPageFromWeb=getFromWeb)
-    
-    c = CatalogueParser()
-    c.LoadParseAndSave(getPageFromWeb=getFromWeb)
-    
-    # The process for fetching transfer data is currently
-    # very manual and is not automated.
-    
-    #TransferParser.transfer_courses_generator()
-    #TransferParser.ahk_data_generator()
-    TransferParser.parse_and_save_transfer_pdfs()
-    
-    
-    c = CourseInfoBuilder()
-    c.hydrateBuildSave()
-    
-    a = AllBuilder()
-    a.hydrateBuildSave()
-
-def UPDATE_ALL(): 
-    p = SemesterParser(2023, 20)
-    p.loadPageFromWeb()
-    p.parseAndSave()
-
-    parse_build_data()
-    sys.exit()
-    
-# WARNING: this takes 9 minutes and 30 seconds to finish on my laptop
-# TODO: make this faster (?)
-UPDATE_ALL() 
+from build import parse_build_data
 
 # refresh current semester every 24 hours
 def daily_update():
     logging.info("Fetching fresh course data from the Langara website.")
-    #p = SemesterParser(2023, 20)
-    #p.loadPageFromWeb()
-    #p.parseAndSave()
+    p = SemesterParser(2023, 20)
+    p.loadPageFromWeb()
+    p.parseAndSave()
 
 
 if not os.path.exists("data/json/"):
