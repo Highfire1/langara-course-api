35e38c874ffb2b9d08424a304da7b2d1394deed6
Anderson T <tseng.andersonn@gmail.com>
2023-06-14 02:23:36 -0700

    gah

--- main.py
+++ main.py
@@ -20,11 +20,13 @@ if not os.path.exists("data/json/"):
     print("No data found. Downloading and parsing all data.")
     parse_build_data(getFromWeb=True)
     
-daily_update()
+#daily_update()
 schedule.every().day.at("00:00:00").do(daily_update)
 
 # uvicorn api.Api:app --host localhost --port 5000 --reload
 if __name__ == "__main__":
+    print("Launching uvicorn.")
+    
     uvicorn.run("api.Api:app", host="0.0.0.0", port=5000)
 
 
